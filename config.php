2c446a660b0e723c6167b4005d51bbbc787e30ad
dc22603 <157369063+dc22603@users.noreply.github.com>
2024-05-05 12:56:09 +0800

    changing port

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-    $conn = new mysqli("localhost:3316","root","","cart_system");
+    $conn = new mysqli("localhost:3316","root","","cart");
     if($conn->connect_error){
         die("Connection Failed!".$conn->connect_error);
     }
